266f1ae288e9cae3a506b970511e5ab67c8cb038
Graeme Lockley <graemel@no9.co.za>
2020-11-01 09:35:17 +0000

    deploy

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-git clone https://git.heroku.com/deno-api-heroku.git
+HEORKU_USER=graemel@no9.co.za
+HEROKU_PASSWORD=0ed8ec8c-859a-4181-bfc1-6f3e8c8d7e86
+
+echo "machine api.heroku.com" > ~/.netrc
+echo "  login $HEROKU_USER" >> ~/.netrc
+echo "  password $HEROKU_PASSWORD" >> ~/.netrc
+echo "machine git.heroku.com" >> ~/.netrc
+echo "  login $HEROKU_USER" >> ~/.netrc
+echo "  password $HEROKU_PASSWORD" >> ~/.netrc
+
+# heroku login
+
+heroku git:clone -a deno-api-heroku
 
 cd deno-api-heroku
 echo "v1.4.6" > runtime.txt
